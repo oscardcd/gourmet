4358c25467f328a766bc523f6a502b4cbd78914c
Sebastian <jsebasrinconpardo@gmail.com>
2020-06-10 19:46:13 -0500

    Actualizacion Carta

---
+++ javascritp/Carta.js
@@ -0,0 +1,8 @@
+function mostrarPasta1() {
+    document.getElementById('pasta2').style.display="none";
+    document.getElementById('pasta1').style.display="block";
+}
+function mostrarPasta2() {
+    document.getElementById('pasta1').style.display="none";
+    document.getElementById('pasta2').style.display="block";
+}
